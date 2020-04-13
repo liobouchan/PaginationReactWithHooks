1d2bf0ab71e048d7aa5bf28934b12468fedf624f
Lio Bouchan <leo.bouchan@gmail.com>
2020-04-13 15:08:20 -0500

    Only receives pages

---
+++ ORIGINAL/src/CommentWithPagination.js
@@ -0,0 +1,39 @@
+import React, { useState, useCallback, useEffect } from "react";
+import PaginationCommon from "./Pagination";
+import Comment from "./Comment";
+import { data } from "./data";
+
+const CommentWithPagination = () => {
+
+  const [currentPage, setCurrentPage] = useState(1);
+
+  let NUM_OF_RECORDS = data.length;
+  let LIMIT = 3;
+
+  const onPageChanged = useCallback(
+    (event, page) => {
+      event.preventDefault();
+      setCurrentPage(page);
+    },
+    [currentPage]
+  );
+
+  const currentComments = data.slice(
+    (currentPage - 1) * LIMIT,
+    (currentPage - 1) * LIMIT + LIMIT
+  );
+
+  return (
+    <div className="pagination-wrapper">
+      {/* <Comment comments={currentComments} /> */}
+      <PaginationCommon
+        totalRecords={NUM_OF_RECORDS}
+        pageLimit={LIMIT}
+        pageNeighbours={1}
+        onPageChanged={onPageChanged}
+        currentPage={currentPage}
+      />
+    </div>
+  );
+};
+export default CommentWithPagination;
